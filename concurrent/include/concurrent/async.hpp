aa06346a9d37acf07f4731dab70e385f2214e060
fugitive-illusion <hayk_khachatryan@yahoo.com>
2022-08-12 14:16:31 +0400

    remove unnecessary comment

--- concurrent/include/concurrent/async.hpp
+++ concurrent/include/concurrent/async.hpp
@@ -21,22 +21,4 @@ auto async(Func func) -> il::future<decltype(func())> {
     return prom->get_future();
 }
 
-// previous version without concepts 
-
-/*template <typename Func, typename... Args>
-auto async(Func func, Args... args) 
-    -> il::future<decltype(func(std::forward<Args>(args)...))> {
-    auto prom = std::make_shared<il::promise<decltype(func(std::forward<Args>(args)...))>>();
-
-    auto& tp = default_tp<std::function<void()>>();
-    tp.put_task(
-        [task_promise = prom, &func, &args...] () mutable {
-            decltype(func(std::forward<Args>(args)...)) res = func(std::forward<Args>(args)...);
-            task_promise->set_value(res);
-        }
-    );
-    return prom->get_future();
-}*/
-
-
-}
+} // namespace il
