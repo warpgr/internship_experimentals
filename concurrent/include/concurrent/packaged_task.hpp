36cd8863565f364c5969cd1404dba88e7ba1455b
Gexam <gexam.karapetyan2000@gmail.com>
2022-08-19 13:46:40 +0400

    packaged_task

--- concurrent/include/concurrent/packaged_task.hpp
+++ concurrent/include/concurrent/packaged_task.hpp
@@ -1,49 +1,106 @@
 #pragma once
 
-#include <future>
+#include <concurrent/future.hpp>
 #include <thread>
+#include <functional>
 
-template <typename func>
-class pack_task;
+namespace il {
 
-template <typename T, typename... Args>
-class pack_task<T(Args...)> {
+template <typename FuncType>
+class packaged_task;
+
+template <typename RetType, typename... Args>
+class packaged_task <RetType(Args...)> {
     public:
-        pack_task () noexcept {}
-        template <typename... Ts>
-        pack_task (Ts &&... ts): fun_ (std::forward<Ts>(ts)...) {} 
-        pack_task (const pack_task& other) = delete;
-        pack_task& operator = (pack_task& other) = delete;
-        pack_task (pack_task&& rhs) noexcept {swap(rhs);}
-        pack_task& operator = (pack_task&& rhs ) {
-             pack_task(std::move(rhs)).swap(*this);
+        packaged_task () noexcept {}
+        template <typename Ts>
+        packaged_task (Ts && func) {
+            fun_ = std::forward<Ts>(func);
+        }
+        packaged_task (const packaged_task& other) = delete;
+        packaged_task& operator = (packaged_task& other) = delete;
+        packaged_task (packaged_task&& rhs) noexcept {swap(rhs);}
+        packaged_task& operator = (packaged_task&& rhs ) {
+             packaged_task(std::move(rhs)).swap(*this);
              return *this;
         }  
     public:
-        void swap(pack_task& other){
-            prom_.swap(other.prom_);
+        void swap(packaged_task& other) {
+            std::swap(prom_, other.prom_);
             fun_.swap(other.fun_);
         }
-
-        template <typename ...Ts>
+        template <typename... Ts>
         void operator()(Ts &&... ts) {
             prom_.set_value(fun_(std::forward<Ts>(ts)...)); 
         }
         
-        std::future<T> get_future() 
+        future<RetType> get_future() 
         { 
             return prom_.get_future(); 
         }
 
         bool valid () const {
-
+            return static_cast<bool>(fun_); 
         }
 
         void reset () {
-            
+            packaged_task(std::move(fun_)).swap(*this);
         } 
 
     private:
-        std::promise<T> prom_;
-        std::function<T(Args...)> fun_;
+        promise<RetType>           prom_;
+        std::function<RetType(Args...)> fun_;
 };
+
+// template<typename> class packaged_task;
+// template< class R, class ...Args > 
+// class packaged_task<R(Args...)>{
+
+//     std::promise<R> p;
+//     std::function<R(Args...)> fn; //use std::function to store function and arguments
+//     public:
+//         packaged_task() noexcept{}
+
+//         //pass function and arguments
+//         template <typename ...Ts>
+//         explicit packaged_task(Ts &&... ts) : fn(std::forward<Ts>(ts)...) { }
+        
+//         //No copy
+//         packaged_task(packaged_task&) = delete;
+//         packaged_task& operator=(packaged_task&) = delete;
+
+//         //move
+//         packaged_task( packaged_task&& rhs ) noexcept{ swap(rhs); }
+//         packaged_task& operator=(packaged_task&& __other)
+//         {
+//           packaged_task(std::move(__other)).swap(*this);
+//           return *this;
+//         }
+        
+//         //pass function and arguments
+//         template <typename ...Ts>
+//         void operator()(Ts &&... ts)
+//         {
+//             p.set_value(fn(std::forward<Ts>(ts)...)); 
+//         }
+
+//         std::future<R> get_future() 
+//         { 
+//             return p.get_future(); 
+//         }
+
+//         void swap(packaged_task& other){
+//             p.swap(other.p);
+//             fn.swap(other.fn);
+//         }
+
+//         bool valid() const{
+//             return static_cast<bool>(fn); 
+//         }
+//        void reset()
+//        {
+//             packaged_task(std::move(fn)).swap(*this);
+//        }
+// };
+
+} //namespace il
