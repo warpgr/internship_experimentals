47f1e9374a3eb101ec7b76b0ac28d4ae8717d610
fugitive-illusion <hayk_khachatryan@yahoo.com>
2022-08-19 18:04:33 +0400

    change memory model in queed_lock.hpp

--- concurrent/include/concurrent/queued_lock.hpp
+++ concurrent/include/concurrent/queued_lock.hpp
@@ -60,16 +60,12 @@ public:
     ticket_lock() = delete;
     ticket_lock(mutex_type& m)
         : m_(m) {
-            // used order_relaxed because we need atomicity guarantee
-            // only for a single operation i.e. fetch_add
             int ticket = ticket_.fetch_add(1, std::memory_order_relaxed);
-            while (ticket != current_pos_);
+            while (ticket != current_pos_.load(std::memory_order_acquire));
             m_.lock();
     }
     ~ticket_lock() {
-        // used order_relaxed because we need atomicity guarantee
-        // only for a single operation i.e. fetch_add
-        current_pos_.fetch_add(1, std::memory_order_relaxed);
+        current_pos_.fetch_add(1, std::memory_order_acq_rel);
         m_.unlock();
     }
     ticket_lock(const ticket_lock& other) = delete;
